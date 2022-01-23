8e40bd76fce4686f40b0bca0bc66068f336038d2
Sandesh Hyoju <sundesz@gmail.com>
2022-01-23 18:51:02 +0200

    Comment code as there is problem with typescript

--- server/src/db/migrations/00_initialize_task_and_user.ts
+++ server/src/db/migrations/00_initialize_task_and_user.ts
@@ -1,56 +1,56 @@
-import { DataTypes, QueryInterface } from "sequelize";
+// import { DataTypes, QueryInterface } from "sequelize";
 
-export {
+// export {
 
-  up: async(queryInterface: QueryInterface) => {
+//   up: async(queryInterface: QueryInterface) => {
 
-    await queryInterface.createTable('tasks',  {
-      taskId: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      isCompleted: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-      },
-    })
+//     await queryInterface.createTable('tasks',  {
+//       taskId: {
+//         type: DataTypes.UUID,
+//         defaultValue: DataTypes.UUIDV4,
+//         primaryKey: true,
+//       },
+//       content: {
+//         type: DataTypes.TEXT,
+//         allowNull: false,
+//       },
+//       isCompleted: {
+//         type: DataTypes.BOOLEAN,
+//         defaultValue: false,
+//       },
+//     })
 
-    await queryInterface.createTable('users',
-            {
-        userId: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          primaryKey: true,
-        },
-        passwordHash: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        username: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
+//     await queryInterface.createTable('users',
+//             {
+//         userId: {
+//           type: DataTypes.UUID,
+//           defaultValue: DataTypes.UUIDV4,
+//           primaryKey: true,
+//         },
+//         passwordHash: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//         },
+//         username: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//           unique: true,
+//         },
+//         name: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//         },
 
-    })
+//     })
 
-    await queryInterface.addColumn('tasks', 'user_id', {
-      type: DataTypes.UUID,
-      allowNull: false,
-      references: {model: 'users', key: 'user_id'},
-    })
-  },
-  down: async (queryInterface: QueryInterface) => {
-    await queryInterface.dropTable('tasks')
-    await queryInterface.dropTable('users')
-  }
-}
+//     await queryInterface.addColumn('tasks', 'user_id', {
+//       type: DataTypes.UUID,
+//       allowNull: false,
+//       references: {model: 'users', key: 'user_id'},
+//     })
+//   },
+//   down: async (queryInterface: QueryInterface) => {
+//     await queryInterface.dropTable('tasks')
+//     await queryInterface.dropTable('users')
+//   }
+// }
