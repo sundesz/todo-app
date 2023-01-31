ae727a4df4fd58655a6f48a943c8a7a5d16988e0
Sandesh Hyoju <sundesz@gmail.com>
2023-01-31 22:39:43 +0200

    Remove unused variable

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-let DATABASE_URL: string;
 let DB_NAME: string;
 let DB_USER: string;
 let DB_PASSWORD: string;
@@ -10,21 +9,18 @@ let DB_HOST: string;
 switch (process.env.NODE_ENV) {
   case 'production':
     DB_NAME = process.env.DB_NAME_PRODUCTION as string;
-    DATABASE_URL = process.env.DATABASE_URL_PRODUCTION as string;
     DB_USER = process.env.DB_USER_PRODUCTION as string;
     DB_PASSWORD = process.env.DB_PASSWORD_PRODUCTION as string;
     DB_HOST = process.env.DB_HOST_PRODUCTION as string;
     break;
   case 'development':
     DB_NAME = process.env.DB_NAME_DEVELOPMENT as string;
-    DATABASE_URL = process.env.DATABASE_URL_DEVELOPMENT as string;
     DB_USER = process.env.DB_USER_DEVELOPMENT as string;
     DB_PASSWORD = process.env.DB_PASSWORD_DEVELOPMENT as string;
     DB_HOST = process.env.DB_HOST_DEVELOPMENT as string;
     break;
   case 'test':
     DB_NAME = process.env.DB_NAME_TEST as string;
-    DATABASE_URL = process.env.DATABASE_URL_TEST as string;
     DB_USER = process.env.DB_USER_TEST as string;
     DB_PASSWORD = process.env.DB_PASSWORD_TEST as string;
     DB_HOST = process.env.DB_HOST_TEST as string;
@@ -39,7 +35,6 @@ const COOKIE_EXPIRE_TIME = (process.env.COOKIE_EXPIRE_TIME ||
   2 * 60 * 60) as number; // 2hours
 
 export {
-  DATABASE_URL,
   PORT,
   SECRET_KEY,
   SALT,
