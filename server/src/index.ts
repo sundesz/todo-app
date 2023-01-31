1397c9319e3334ca45516805701d18e1c1addca9
Sandesh Hyoju <sundesz@gmail.com>
2023-01-31 22:31:38 +0200

    Refactor code

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,20 @@ import app from './app';
 import { connectToDatabase } from './db';
 
 import { PORT } from './config';
+import { logErrorMessage } from './utils/loggers';
 
 const server = http.createServer(app);
 
+export const SRC_DIR = __dirname;
+
 const start = async () => {
-  await connectToDatabase();
-  server.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+  try {
+    await connectToDatabase();
+    server.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+  } catch (error: unknown) {
+    logErrorMessage(error);
+    console.log(error);
+  }
 };
 
 void start();
