61fc91992dcb6e8b8caa29cf8680833c8bc3feff
Sandesh Hyoju <sundesz@gmail.com>
2023-02-01 23:36:08 +0200

    Add errorMessage

---
+++ server/src/utils/errorMessage.ts
@@ -0,0 +1,5 @@
+export const errMessage = {
+  REQUIRED_USER_PASS: 'Username and password are required.',
+  INVALID_USER_PASS: 'Invalid username or password.',
+  INVALID_TOKEN: 'Invalid Token.',
+};
