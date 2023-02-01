d98953bcd972c2047cc38dcc1e65e30beefb6361
Sandesh Hyoju <sundesz@gmail.com>
2023-02-01 23:34:58 +0200

    Code refactor

--- server/src/middleware/helper.ts
+++ server/src/middleware/helper.ts
@@ -1,42 +1,46 @@
 import { NextFunction, RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
-import cookie from 'cookie';
 import { Task, User } from '../db/models';
-import { SECRET_KEY } from '../config';
+import {
+  ACCESS_TOKEN_EXPIRE_TIME,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRE_TIME,
+  REFRESH_TOKEN_SECRET,
+} from '../config';
+import { AccessTokenType, RefreshTokenType } from '../types';
 
 /**
- * Extract token from request
+ * Verify Access Token and validate user
  */
-export const tokenExtractor: RequestHandler = (
+export const verifyAccessToken: RequestHandler = async (
   req,
   res,
   next: NextFunction
 ) => {
-  const cookieToken = cookie.parse(req.headers.cookie || '');
-
   try {
-    if (!cookieToken.auth && req.url === '/refresh') {
-      return res.status(204).end();
+    const authHeader = req.headers.authorization;
+    if (!authHeader?.toLowerCase().startsWith('bearer')) {
+      return res.sendStatus(401);
     }
-    req.decodedToken = jwt.verify(cookieToken.auth, SECRET_KEY);
+
+    const token = authHeader.substring(7);
+    const decodedToken = jwt.verify(
+      token,
+      ACCESS_TOKEN_SECRET
+    ) as AccessTokenType;
+
+    const foundUser = await getUser(decodedToken.userInfo.username);
+
+    if (!foundUser) {
+      return res.sendStatus(401);
+    }
+
+    req.decodedToken = {
+      username: foundUser.username,
+      userId: foundUser.userId,
+    };
   } catch (error) {
     next(error);
-  }
-
-  next();
-};
-
-/**
- * Checks if user is a valid or not
- */
-export const isValidUser: RequestHandler = async (req, res, next) => {
-  const { id } = req.decodedToken as { id: string };
-  const user = await User.findByPk(id);
-
-  if (!user) {
-    return res
-      .status(401)
-      .json({ error: 'Invalid Token. Please login again.' });
   }
 
   next();
@@ -46,14 +50,71 @@ export const isValidUser: RequestHandler = async (req, res, next) => {
  * Finds tasks by taskId
  */
 export const taskFinder: RequestHandler = async (req, res, next) => {
-  const { id: taskId } = req.params as { id: string };
-  const task = await Task.findByPk(taskId);
+  try {
+    const { id: taskId } = req.params as { id: string };
+    const task = await Task.findByPk(taskId);
 
-  if (task) {
+    if (!task) {
+      return res.sendStatus(404);
+    }
+
     req.task = task;
-  } else {
-    return res.status(404).end();
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Find one user by username
+ */
+export const getUser = async (username: string) => {
+  return await User.findOne({
+    where: { username, isActive: true },
+    attributes: ['userId', 'name', 'username', 'passwordHash', 'role'],
+    include: {
+      model: Task,
+      attributes: ['taskId', 'content', 'isCompleted', 'important'],
+    },
+    order: [[Task, 'createdAt', 'asc']],
+  });
+};
+
+/**
+ * Generates access token
+ */
+export const generateAccessToken = (user: User) => {
+  if (user === null) {
+    throw new Error('User not found');
   }
 
-  next();
+  const tokenObj: AccessTokenType = {
+    userInfo: {
+      name: user.name,
+      username: user.username,
+      userId: user.userId,
+      role: user.role,
+    },
+  };
+
+  return jwt.sign(tokenObj, ACCESS_TOKEN_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRE_TIME,
+  });
+};
+
+/**
+ * Generates access and refresh token
+ */
+export const generateRefreshToken = (user: User) => {
+  if (user === null) {
+    throw new Error('User not found');
+  }
+
+  const tokenObj: RefreshTokenType = {
+    username: user.username,
+  };
+
+  return jwt.sign(tokenObj, REFRESH_TOKEN_SECRET, {
+    expiresIn: REFRESH_TOKEN_EXPIRE_TIME,
+  });
 };
--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -1,9 +1,9 @@
 import { RequestHandler } from 'express';
 import { errorHandler } from './errorHandler';
-import { tokenExtractor, isValidUser } from './helper';
+import { verifyAccessToken } from './helper';
 
 const unknownEndpoint: RequestHandler = (req, res) => {
   res.status(404).json({ error: `Unknown Endpoint ${req.originalUrl}` });
 };
 
-export { errorHandler, unknownEndpoint, tokenExtractor, isValidUser };
+export { errorHandler, unknownEndpoint, verifyAccessToken };
